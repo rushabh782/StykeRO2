b991fdea33fba7dee4c508acb628429c6e682149
rushabh782 <rushabhpatel877@gmail.com>
2024-10-06 13:58:56 +0530

    added all files

---
+++ components/FancyCard.tsx
@@ -0,0 +1,82 @@
+import {Image, StyleSheet, Text, View} from 'react-native';
+import React from 'react';
+
+export default function FancyCard() {
+  return (
+    <View>
+      <Text style={styles.headingText}>FancyCard</Text>
+      <View style={[styles.card, styles.cardElevated]}>
+        <Image
+          source={{
+            uri: 'https://theheritageart.com/wp-content/uploads/2022/11/hawa-mahal.jpg',
+          }}
+          style={styles.cardImage}
+        />
+        <View style={styles.cardBody}>
+          <Text style={styles.cardTitle}>Hawa Mahal</Text>
+          <Text style={styles.cardLabel}>Pink City, Jaipur</Text>
+          <Text style={styles.cardDescription}>
+            The Hawa Mahal is the palace in the city of Jaipur,India. Built from
+            red and pink sandstone,it is on the edge of City Palace
+          </Text>
+          <Text style={styles.cardFooter}>12 min away</Text>
+        </View>
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  headingText: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    paddingHorizontal: 8,
+  },
+  card: {
+    width: 350,
+    height: 360,
+    borderRadius: 6,
+    marginVertical: 12,
+    marginHorizontal: 16,
+  },
+  cardElevated: {
+    backgroundColor: '#FFFFFF',
+    elevation: 3,
+    shadowOffset: {
+      width: 1,
+      height: 1,
+    },
+  },
+  cardImage: {
+    height: 180,
+    marginBottom: 8,
+    borderTopLeftRadius: 6,
+    borderTopRightRadius: 6,
+  },
+  cardBody: {
+    flex: 1,
+    flexGrow: 1,
+    paddingHorizontal: 12,
+  },
+  cardTitle: {
+    color: '#000000',
+    fontSize: 22,
+    fontWeight: 'bold',
+    marginBottom: 4,
+  },
+  cardLabel: {
+    color: '#000000',
+    fontSize: 14,
+    marginBottom: 6,
+  },
+  cardDescription: {
+    color: '#2F363F',
+    marginBottom: 12,
+    fontSize: 12,
+    marginTop: 6,
+    flexShrink: 1,
+  },
+  cardFooter: {
+    color: '#000000',
+  },
+});
